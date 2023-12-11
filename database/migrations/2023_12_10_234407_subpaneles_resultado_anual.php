184195af7bde973f84a1223305928ae2b8962243
Francisco Bórquez <fborquez@outlook.com>
2023-12-11 20:36:39 -0300

    fix

--- database/migrations/2023_12_10_234407_subpaneles_resultado_anual.php
+++ database/migrations/2023_12_10_234407_subpaneles_resultado_anual.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class SubpanelesReporteAnual extends Migration
+class CreateSubpanelesReporteAnual extends Migration
 {
     /**
      * Run the migrations.
