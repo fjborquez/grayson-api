07616d2600ae435d295daf7aa83c6a32aca27376
Francisco Borquez <fborquez@outlook.com>
2024-01-01 22:57:49 -0300

    fix

--- app/Models/Dato.php
+++ app/Models/Dato.php
@@ -5,12 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Dato extends Model
 {
     use HasFactory;
     protected $table = 'datos';
     protected $fillable = ['clave', 'valor', 'serie_id'];
+
+    protected function valor(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => intval($value)
+        );
+    }
 
     public function series(): BelongsTo
     {
